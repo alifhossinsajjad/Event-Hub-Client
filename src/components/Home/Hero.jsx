bc6dc16cc6b86360dc77ce0fb99a20b03a42e649
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-11-26 22:05:22 +0600

    change the file name

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import bgImg from "../../assets/event1.avif";
+import bgImg from "../../assets/event1.jpg";
 import Link from "next/link";
 
 const HeroSection = () => {
